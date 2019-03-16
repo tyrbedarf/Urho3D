46567528f311db6c145312ef2e8da1f47fd09b82
Dirk Becker <tyr@tyrbedarf.de>
2019-03-16 19:22:08 +0100

    WIP

--- Source/Samples/85_JsonSerializer/HelloJsonSerialize.h
+++ Source/Samples/85_JsonSerializer/HelloJsonSerialize.h
@@ -1,6 +1,5 @@
 //
-// Copyright (c) 2008-2016 the Urho3D project.
-// Copyright (c) 2014-2016, THUNDERBEAST GAMES LLC All rights reserved
+// Copyright (c) 2008-2019 the Urho3D project.
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
